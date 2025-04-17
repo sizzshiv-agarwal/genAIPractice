78d4cb886809443604a272e5be759919a7e4e4e7
Shivangi.Agarwal <shivangiagarwal@outlook.com>
2025-04-17 16:21:18 +0530

    Initial commit

---
+++ geminichat.py
@@ -0,0 +1,12 @@
+from google import genai
+from google.genai import types
+client = genai.Client(api_key='AIzaSyC2G5X7kG7Ve-jQqHfoglXDAnQ8SOSqx6w')
+
+# Only run this block for Vertex AI API
+#client = genai.Client(
+ #   vertexai=True, project='988710364027', location='us-central1'
+
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001', contents='Why is the sky blue?'
+)
+print(response.text)
